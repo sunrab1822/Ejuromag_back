86c3d2b8b26830614ccecbc92d63c077be7c45b1
sunrab1822 <87667419+sunrab1822@users.noreply.github.com>
2024-03-11 09:51:43 +0100

    fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,13 +15,13 @@ class ProductController extends Controller
         return response()->json(["error" => false, "data" => $categorywithproducts]);
     }
 
-    public funtion getProduct($id){
+    public function getProduct($id){
         $product = where(Product::where('id', $id)->get());
         if (!$product) {
             return response()->json(["error" => true, "message" => "A termék nem található"]);
         }
 
-        return  return response()->json(["error" => false, "data" => $product]);
+        return response()->json(["error" => false, "data" => $product]);
 
     }
 
